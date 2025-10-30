be6bf04841476e6b6788498df0306ef2c22d609c
Hennadi Kutomanov <hkutomanov@xsightsys.com>
2025-10-30 19:21:01 +0200

    Сщккусе test_imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,3 +3,6 @@ def test_imports():
     import opmed.dataloader
     import opmed.solver_core
 
+    # проверяем, что модули реально загрузились
+    assert all([opmed, opmed.dataloader, opmed.solver_core])
+
